8d324d140a8a8042ed0dd68624075dbed8bad65c
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-15 17:27:38 -0400

    :sparkles: new planet

--- src/components/solar-system/index.tsx
+++ src/components/solar-system/index.tsx
@@ -4,21 +4,21 @@ import { Planet } from "./planet";
 //import { Stars } from "./stars";
 import { Sun } from "./sun";
 
-//<ambientLight position={[1, 1, 1]} />
 //<Sun />
 export function SolarSystem() {
   return (
     <Canvas
       camera={{
-        position: [0, 120, 90],
-        rotation: [-1, 0, 0],
-        fov: 70,
+        position: [0, 50, 80],
+        fov: 60,
       }}
     >
+      <ambientLight position={[1, 1, 1]} />
       <Sun radius={5} />
       <Planet distance={8} velocity={1} size={2} />
       <Planet distance={6} velocity={1} size={2} />
       <Planet distance={10} velocity={1} size={2} />
+      <Planet distance={12} velocity={1} size={2} />
       <OrbitControls />
       <gridHelper scale={[10, 0, 10]} />
     </Canvas>
