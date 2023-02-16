4559b5d92752c622303970950e64d8121a60abdf
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-16 19:39:12 -0400

    :sparkles: ring for planets added

--- src/components/solar-system/planet.tsx
+++ src/components/solar-system/planet.tsx
@@ -2,24 +2,29 @@ import { MeshProps, useFrame, useLoader } from "@react-three/fiber";
 import { useRef } from "react";
 import { BufferGeometry, Euler, Material, Mesh, TextureLoader } from "three";
 import { Orbit } from "./orbit";
+import { Ring } from "./ring";
 
 interface PlanetsProps extends MeshProps {
   distance: number;
   velocity: number;
   size: number;
   texture_name?: string;
+  ring?: boolean;
 }
 export function Planet({
   distance = 20,
   velocity = 1,
   size = 1,
   texture_name,
+  ring,
   ...props
 }: PlanetsProps) {
   const ref = useRef<Mesh<BufferGeometry, Material | Material[]>>(null);
 
-  const orbitRadius = 2 * distance;
+  const ringDistance = size * 1.5;
+  const orbitRadius = 2 * distance + size / 2;
   const rotation = new Euler(-1.5, 0, 0);
+
   let texture = null;
   if (texture_name) {
     texture = useLoader(TextureLoader, `/${texture_name}.jpg`);
@@ -44,6 +49,11 @@ export function Planet({
       <mesh {...props} ref={ref} position={[orbitRadius, 0, 0]}>
         <sphereGeometry args={[size, 10, 10]} />
         <meshBasicMaterial color={texture ? "" : "blue"} map={texture} />
+        {ring ? (
+          <Ring distance={ringDistance} depth={0.1} rotation={rotation} />
+        ) : (
+          <></>
+        )}
       </mesh>
       <Orbit orbit={orbitRadius} rotation={rotation} />
     </>
