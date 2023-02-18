74e2b11ab754ca6e206ae1259c1b623d930dbaf7
matheusgmc <matheus.geraldo1@outlook.com>
2023-02-17 20:04:54 -0400

    :sparkles: new distance calc

--- src/components/solar-system/planet.tsx
+++ src/components/solar-system/planet.tsx
@@ -5,24 +5,28 @@ import { Orbit } from "./orbit";
 import { Ring } from "./ring";
 
 interface PlanetsProps extends MeshProps {
-  distance: number;
+  position: number;
   velocity: number;
   size: number;
   texture_name?: string;
+  multiply_orbit?: number;
   ring?: boolean;
 }
 export function Planet({
-  distance = 20,
+  position = 20,
   velocity = 1,
   size = 1,
   texture_name,
   ring,
+  multiply_orbit,
   ...props
 }: PlanetsProps) {
   const ref = useRef<Mesh<BufferGeometry, Material | Material[]>>(null);
 
   const ringDistance = size * 1.5;
-  const orbitRadius = 2 * distance + size / 2;
+
+  const distance = 2 * size * position * (multiply_orbit || 2);
+
   const rotation = new Euler(-1.5, 0, 0);
 
   let texture = null;
@@ -37,17 +41,17 @@ export function Planet({
     orbit += delta * velocity;
     return (
       ref.current.position.set(
-        Math.cos(orbit) * orbitRadius,
+        Math.cos(orbit) * distance,
         0,
-        Math.sin(orbit) * orbitRadius
+        Math.sin(orbit) * distance
       ),
       (ref.current.rotation.y += delta * 0.5)
     );
   });
   return (
     <>
-      <mesh {...props} ref={ref} position={[orbitRadius, 0, 0]}>
-        <sphereGeometry args={[size, 10, 10]} />
+      <mesh {...props} ref={ref} position={[distance, 0, 0]}>
+        <sphereGeometry args={[size, 20, 20]} />
         <meshBasicMaterial color={texture ? "" : "blue"} map={texture} />
         {ring ? (
           <Ring distance={ringDistance} depth={0.1} rotation={rotation} />
@@ -55,7 +59,7 @@ export function Planet({
           <></>
         )}
       </mesh>
-      <Orbit orbit={orbitRadius} rotation={rotation} />
+      <Orbit orbit={distance} rotation={rotation} />
     </>
   );
 }
